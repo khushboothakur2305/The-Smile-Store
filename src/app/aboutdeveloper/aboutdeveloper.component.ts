c627345b7514239a733f410b88685ac0493dbe80
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-08-29 23:13:59 +0530

    update

--- src/app/aboutdeveloper/aboutdeveloper.component.ts
+++ src/app/aboutdeveloper/aboutdeveloper.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-aboutdeveloper',
   templateUrl: './aboutdeveloper.component.html',
-  styleUrls: ['./aboutdeveloper.component.scss']
+  styleUrls: ['./aboutdeveloper.component.scss'],
 })
 export class AboutdeveloperComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
